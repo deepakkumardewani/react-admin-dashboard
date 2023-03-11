df6eff4498781b06b38b93ba1a9fcce0fb8e1705
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-03-11 23:25:22 +0530

    ADD(components): SparkLIne, Stacked, Ecommerce

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,44 @@
+import {
+  Category,
+  ChartComponent,
+  Inject,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 import React from "react";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+import { useStateContext } from "../../contexts/ContextProvider";
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <ChartComponent
+      id='charts'
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{ background: "white" }}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
